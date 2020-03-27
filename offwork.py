234c0f38f1f561f025495326f8d667b49d6dff5b
LittleMO <pp8323127@gmail.com>
2020-03-27 18:34:50 +0800

    text

--- offwork.py
+++ offwork.py
@@ -7,7 +7,7 @@ import requests
 
 #content="\n胖熊胖熊胖熊胖熊胖熊胖熊胖熊胖熊胖熊胖熊胖熊胖熊胖熊胖胖雄熊肉熊肉熊肉熊肉熊肉熊肉熊肉熊肉熊肉熊肉熊肉熊肉熊肉熊肉熊肉熊\n哥\n哥\n快\n去\n"+"收\n店\n！"
 
-content="\n\n|￣￣￣￣￣￣￣￣￣|\n| 胖肉熊哥哥去收店 ! |\n|＿＿＿＿＿＿＿＿＿|\n        (\__/)  ||\n        (•ㅅ•) ||\n        / 　 づ"
+content="\n\n|￣￣￣￣￣￣￣￣￣ |\n|  胖肉熊哥哥去收店 ! |\n|＿＿＿＿＿＿＿＿＿ |\n        (\__/)  ||\n        (•ㅅ•) ||\n        / 　 づ"
 
 def lineNotifyMessage(token, msg):
     headers = {
@@ -18,6 +18,6 @@ def lineNotifyMessage(token, msg):
     r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
     return r.status_code
 
-token = '6ZuSFknc3D9goTQMouQ7O1Cm7nQMcKeKthyl6v4vsz0' #修改為自己的權杖內容，肉熊
-#token = 'dK81KKbB0bTe0dVWAohdZgJ5IkMYT7raeOKOpeHfUuj' #修改為自己的權杖內容，個人
+#token = '6ZuSFknc3D9goTQMouQ7O1Cm7nQMcKeKthyl6v4vsz0' #修改為自己的權杖內容，肉熊
+token = 'dK81KKbB0bTe0dVWAohdZgJ5IkMYT7raeOKOpeHfUuj' #修改為自己的權杖內容，個人
 lineNotifyMessage(token, content)
